ecb7d5835cc7195da802fc480095c20b3d1489c9
杜胜超 <dusc@zzjgtl.com>
2021-04-13 16:36:10 +0800

    第一次提交

---
+++ build-mysql/privileges.sql
@@ -0,0 +1,7 @@
+use mysql;
+select host, user from user;
+-- 因为mysql版本是5.7，因此新建用户为如下命令：
+-- 将docker_mysql数据库的权限授权给创建的docker用户，密码为123456：
+grant all on *.* to root@'%' identified by 'mysql' with grant option;
+-- 这一条命令一定要有：
+flush privileges;
